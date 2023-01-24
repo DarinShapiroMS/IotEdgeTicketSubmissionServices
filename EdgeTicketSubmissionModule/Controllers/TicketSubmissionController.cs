8dd55840b553d01db9e5ba359e74e1d98f2aed47
Darin Shapiro <dashapir@microsoft.com>
2023-01-23 18:23:51 -0800

    Add project files.

---
+++ EdgeTicketSubmissionModule/Controllers/TicketSubmissionController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EdgeTicketSubmissionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketSubmissionController : ControllerBase
+    {
+        // GET: api/<TicketSubmissionController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TicketSubmissionController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Posting a valid JSON document to this controller sends the JSON
+        /// document to IoT Hub, where further message routing will send
+        /// the JSON document to Cosmos DB. 
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public async void Post([FromBody] JsonObject value)
+        {
+            // as long as we got a json document, send
+            // upstream through IoT Hub
+            
+            // TODO:  Do we need to added a time stamp for when the ticket
+            // was submitted to this local ticket submission service?
+
+            string strPayload = System.Text.Json.JsonSerializer.Serialize(value);
+            var bytes = System.Text.Encoding.UTF8.GetBytes(strPayload);
+            var message = new Message(bytes);
+
+            message.ContentEncoding = "utf-8";
+            message.ContentType = "application/json";
+
+            await Program.IoTHubModuleClient.SendEventAsync("output1", message);            
+        }
+
+        // PUT api/<TicketSubmissionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TicketSubmissionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EdgeTicketSubmissionModule/Program.cs
@@ -0,0 +1,117 @@
+using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+using System.Diagnostics.Metrics;
+using System.Runtime.Loader;
+using System.Text;
+
+namespace EdgeTicketSubmissionService
+
+{
+    public class Program
+    {
+
+        static int counter;
+        private static ModuleClient ioTHubModuleClient;
+
+        internal static ModuleClient IoTHubModuleClient { get => ioTHubModuleClient; }
+
+        public static void Main(string[] args)
+        {
+            // Initialize IoT Edge's module client
+            Init().Wait();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+
+
+        }
+
+        /// <summary>
+        /// Handles cleanup operations when app is cancelled or unloads
+        /// </summary>
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Initializes the ModuleClient and sets up the callback to receive
+        /// messages containing temperature information
+        /// </summary>
+        static async Task Init()
+        {
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            ITransportSettings[] settings = { mqttSetting };
+
+            // Open a connection to the Edge runtime
+            ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await IoTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
+
+            // Register callback to be called when a message is received by the module
+            await IoTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, IoTHubModuleClient);
+        }
+
+        /// <summary>
+        /// We're not using module to module messaging here yet, just simply
+        /// exposing a REST api to the local device host (EFLOW vm and Windows host). 
+        /// 
+        /// This method is called whenever the module is sent a message from the EdgeHub. 
+        /// It just pipe the messages without any change.
+        /// It prints all the incoming messages.
+        /// </summary>
+        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        {
+            int counterValue = Interlocked.Increment(ref counter);
+
+            var moduleClient = userContext as ModuleClient;
+            if (moduleClient == null)
+            {
+                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+            }
+
+            byte[] messageBytes = message.GetBytes();
+            string messageString = Encoding.UTF8.GetString(messageBytes);
+            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+
+            if (!string.IsNullOrEmpty(messageString))
+            {
+                using (var pipeMessage = new Message(messageBytes))
+                {
+                    foreach (var prop in message.Properties)
+                    {
+                        pipeMessage.Properties.Add(prop.Key, prop.Value);
+                    }
+                    await moduleClient.SendEventAsync("output1", pipeMessage);
+
+                    Console.WriteLine("Received message sent");
+                }
+            }
+            return MessageResponse.Completed;
+        }
+    }
+
+
+}
